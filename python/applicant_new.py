3d6a8674b1f89999aeec7c5eea2708693748b7eb
ssovago <sandor.sovago@emarsys.com>
2022-08-26 15:46:16 +0200

    Add new applicant class

---
+++ python/applicant_new.py
@@ -0,0 +1,19 @@
+
+class Applicant:
+
+    def _init_(self, ranked_contracts):
+        self._ranked_contracts = ranked_contracts
+        self._admitted = None
+        self._rejected = None
+
+    def is_proposer(self):
+        pass
+
+    def set_admitted(self):
+        pass
+
+    def set_rejected(self):
+        pass
+
+    def get_next_application(self):
+        pass
