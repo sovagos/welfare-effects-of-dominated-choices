fe1461302c53048080e90cd315fe50b593053244
probalazs <probalazs@nyenyere.eu>
2022-10-07 13:32:19 +0200

    Copy old e2e tests of calculate to get contracts.

--- tests/unit/get_contracts/test_get_contracts.py
+++ tests/unit/get_contracts/test_get_contracts.py
@@ -3,13 +3,179 @@ import pytest
 from python.get_contracts.get_contracts import (
     get_contracts,
 )
+from tests.unit.helpers import (
+    create_contract_with_score,
+    create_contract,
+    create_applicant,
+    create_application,
+)
 
 
 use_cases = [
     {
-        "contracts": [],
-        "applicants": [],
-        "expected": [],
+        "contracts": [
+            create_contract({"id": "C1", "capacity": 1}),
+            create_contract({"id": "C2", "capacity": 1}),
+            create_contract({"id": "C3", "capacity": 1}),
+        ],
+        "applicants": [
+            create_applicant(
+                {
+                    "id": "A1",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 10}),
+                        create_application({"contract": "C2", "priority_score": 10}),
+                        create_application({"contract": "C3", "priority_score": 11}),
+                    ],
+                }
+            )
+        ],
+        "expected": [
+            create_contract_with_score({"id": "C1", "score": 10}),
+            create_contract_with_score({"id": "C2", "score": 0}),
+            create_contract_with_score({"id": "C3", "score": 0}),
+        ],
+    },
+    {
+        "contracts": [
+            create_contract({"id": "C1", "capacity": 1}),
+            create_contract({"id": "C2", "capacity": 1}),
+        ],
+        "applicants": [
+            create_applicant(
+                {
+                    "id": "A1",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 11}),
+                        create_application({"contract": "C2", "priority_score": 11}),
+                    ],
+                },
+            ),
+            create_applicant(
+                {
+                    "id": "A2",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 9}),
+                    ],
+                }
+            ),
+        ],
+        "expected": [
+            create_contract_with_score({"id": "C1", "score": 11}),
+            create_contract_with_score({"id": "C2", "score": 0}),
+        ],
+    },
+    {
+        "contracts": [
+            create_contract({"id": "C1", "capacity": 1}),
+            create_contract({"id": "C2", "capacity": 1}),
+            create_contract({"id": "C3", "capacity": 1}),
+        ],
+        "applicants": [
+            create_applicant(
+                {
+                    "id": "A1",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 10}),
+                        create_application({"contract": "C2", "priority_score": 10}),
+                        create_application({"contract": "C3", "priority_score": 10}),
+                    ],
+                },
+            ),
+            create_applicant(
+                {
+                    "id": "A2",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 11}),
+                    ],
+                }
+            ),
+            create_applicant(
+                {
+                    "id": "A3",
+                    "ranked_applications": [
+                        create_application({"contract": "C2", "priority_score": 11}),
+                    ],
+                }
+            ),
+        ],
+        "expected": [
+            create_contract_with_score({"id": "C1", "score": 11}),
+            create_contract_with_score({"id": "C2", "score": 11}),
+            create_contract_with_score({"id": "C3", "score": 10}),
+        ],
+    },
+    {
+        "contracts": [
+            create_contract({"id": "C1", "capacity": 1}),
+            create_contract({"id": "C2", "capacity": 1}),
+            create_contract({"id": "C3", "capacity": 1}),
+        ],
+        "applicants": [
+            create_applicant(
+                {
+                    "id": "A1",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 9}),
+                    ],
+                },
+            ),
+            create_applicant(
+                {
+                    "id": "A2",
+                    "ranked_applications": [
+                        create_application({"contract": "C2", "priority_score": 10}),
+                    ],
+                }
+            ),
+            create_applicant(
+                {
+                    "id": "A3",
+                    "ranked_applications": [
+                        create_application({"contract": "C3", "priority_score": 11}),
+                    ],
+                }
+            ),
+        ],
+        "expected": [
+            create_contract_with_score({"id": "C1", "score": 9}),
+            create_contract_with_score({"id": "C2", "score": 10}),
+            create_contract_with_score({"id": "C3", "score": 11}),
+        ],
+    },
+    {
+        "contracts": [
+            create_contract({"id": "C1", "capacity": 2}),
+        ],
+        "applicants": [
+            create_applicant(
+                {
+                    "id": "A1",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 9}),
+                    ],
+                },
+            ),
+            create_applicant(
+                {
+                    "id": "A2",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 10}),
+                    ],
+                }
+            ),
+            create_applicant(
+                {
+                    "id": "A3",
+                    "ranked_applications": [
+                        create_application({"contract": "C1", "priority_score": 11}),
+                    ],
+                }
+            ),
+        ],
+        "expected": [
+            create_contract_with_score({"id": "C1", "score": 10}),
+        ],
     },
 ]
 
