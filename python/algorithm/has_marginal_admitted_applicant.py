21398324b7b0d41d1673bbf47c83f65701bcb50f
probalazs <probalazs@nyenyere.eu>
2022-10-07 11:54:12 +0200

    Remove ol algorithm folder.

--- python/algorithm/has_marginal_admitted_applicant.py
+++
@@ -1,5 +0,0 @@
-from python.algorithm.contract import Contract
-
-
-def has_marginal_admitted_applicant(contract: Contract) -> bool:
-    return len(contract.admitted_applicants) == contract.capacity
