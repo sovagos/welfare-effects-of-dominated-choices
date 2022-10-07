0fca6fc5def2edbfd0e9fdd8c046926a4ba6556b
probalazs <probalazs@nyenyere.eu>
2022-10-07 13:06:59 +0200

    Create dummy test for get contracts.

---
+++ tests/unit/get_contracts/test_get_contracts.py
@@ -0,0 +1,20 @@
+import pytest
+
+from python.get_contracts.get_contracts import (
+    get_contracts,
+)
+
+
+use_cases = [
+    {
+        "contracts": [],
+        "applicants": [],
+        "expected": [],
+    },
+]
+
+@pytest.mark.parametrize("use_case", use_cases)
+def test__get_contracts(use_case) -> None:
+    result = get_contracts(contracts=use_case["contracts"], applicants=use_case["applicants"])
+
+    assert result == use_case["expected"]
