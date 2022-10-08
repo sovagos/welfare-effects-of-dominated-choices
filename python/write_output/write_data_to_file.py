ddda378a576a96141042e9d14c622b279a8adcc3
probalazs <probalazs@nyenyere.eu>
2022-10-08 13:59:49 +0200

    Csv file writer created.

---
+++ python/write_output/write_data_to_file.py
@@ -0,0 +1,8 @@
+from csv import writer
+
+
+def write_data_to_file(file: str, data: list[list[str]]) -> None:
+    with open(file, 'w+', newline='') as csvfile:
+        spamwriter = writer(csvfile, delimiter=',')
+        for row in data:
+            spamwriter.writerow(row)
---
+++ tests/integration/write_output/test_write_data_to_file.py
@@ -0,0 +1,27 @@
+from os import path
+from pathlib import Path
+from csv import reader
+
+from python.write_output.write_data_to_file import write_data_to_file
+from tests.unit.helpers import get_random_string
+
+OUTPUT_FOLDER = file = path.join(Path(__file__).parent.resolve(), "output",)
+
+def test__write_data_to_file__writes_the_give_data_to_the_given_file() -> None:
+    data = [
+        ["first", "second", "third"],
+        ["1", "2", "3"]
+    ]
+    file = path.join(OUTPUT_FOLDER, f"{get_random_string()}.csv")
+
+    write_data_to_file(file=file, data=data)
+
+    result = get_data_from_file(file=file)
+    assert result == data
+
+def get_data_from_file(file: str) -> list[list[str]]:
+    result = []
+    with open(file) as csvfile:
+        for row in reader(csvfile):
+            result.append(row)
+    return result
